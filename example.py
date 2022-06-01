a79c6a146c2685fd7a61dc078274f0674d0f85c0
mrjakobdk <Vebl8550>
2022-06-01 15:10:56 +0200

    added coopertive groups

--- example.py
+++ example.py
@@ -1,29 +1,52 @@
 import numpy as np
 import DBSCAN_GPU as gpu
 from sklearn import datasets
+from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import time
 import torch
+import torchvision.datasets as torch_datasets
+import umap
 
-iris = datasets.load_iris()
-data = np.asarray(iris.data[:, :], dtype=np.float32)
+# loading iris
+# iris = datasets.load_iris()
+# data = np.asarray(iris.data[:, :], dtype=np.float32)
+
+# loading digits
+# digits = datasets.load_digits()
+# data = np.asarray(digits.data[:, :], dtype=np.float32)
+#
+# pca = PCA(n_components=2)
+# data = np.asarray(pca.fit_transform(data), dtype=np.float32, order="c")
+
+# loading mnist
+mnist = torch_datasets.MNIST(root="data", train=True, download=True, transform=None)
+data = np.asarray(mnist.data[:, :], dtype=np.float32)
+data = data.reshape(data.shape[0], data.shape[1] * data.shape[2])
+
+# reducer = PCA(n_components=2)
+reducer = umap.UMAP()
+data = np.asarray(reducer.fit_transform(data), dtype=np.float32, order="c")
+a = 0.1
+plt.scatter(data[:, 0], data[:, 1], alpha=a)
+plt.show()
+
+eps = .5  # 2.5
+minPts = 400
 
 torch.cuda.synchronize()
 
+# t0 = time.time()
+# C = gpu.GPU_DBSCAN(data, eps=eps, minPts=minPts)
+# t1 = time.time()
+# print(t1 - t0)#, C, np.unique(C, return_counts=True))
+# plt.scatter(data[:, 0], data[:, 1], c=C, alpha=a)
+# plt.show()
+
+
 t0 = time.time()
-C = gpu.GPU_DBSCAN(data, eps=.25, minPts=5)
+C = gpu.G_DBSCAN(data, eps=eps, minPts=minPts)
 t1 = time.time()
-
-print(t1-t0, C)
-
-plt.scatter(data[:, 0], data[:, 1], c=C)
-plt.show()
-
-t0 = time.time()
-C = gpu.G_DBSCAN(data, eps=.25, minPts=5)
-t1 = time.time()
-
-print(t1-t0, C)
-
-plt.scatter(data[:, 0], data[:, 1], c=C)
+print(t1 - t0)#, C, np.unique(C, return_counts=True))
+plt.scatter(data[:, 0], data[:, 1], c=C, alpha=a)
 plt.show()
--- src/algorithms/GPU_DBSCAN.cu
+++ src/algorithms/GPU_DBSCAN.cu
@@ -2,9 +2,12 @@
 // Created by jakobrj on 5/31/22.
 //
 #import <stdio.h>
+#include <cooperative_groups.h>
 
 #include "GPU_DBSCAN.cuh"
 #include "../utils/GPU_utils.cuh"
+
+namespace cg = cooperative_groups;
 
 #define BLOCK_SIZE 1024
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
@@ -25,26 +28,27 @@ __device__ int get_end(const int *d_array, const int idx) {
 }
 
 __device__
-float euclidian_distance(int i_point, int j_point, float *d_data, int d) {
-    float *p = &d_data[i_point * d];
-    float *q = &d_data[j_point * d];
+float euclidian_distance(int i_point, int j_point, const float *__restrict__ d_data, int d) {
     double distance = 0;
     for (int i_dimension = 0; i_dimension < d; i_dimension++) {
-        double diff = p[i_dimension] - q[i_dimension];
+        double diff = d_data[i_point * d + i_dimension] - d_data[j_point * d + i_dimension];
         distance += diff * diff;
     }
     return sqrt(distance);
 }
 
 __global__
-void kernel_neighborhood_sizes(int *d_neighborhood_sizes, float *d_data, int n, int d, float eps) {
+void kernel_neighborhood_sizes(int *__restrict__ d_neighborhood_sizes, const float *__restrict__ d_data, int n, int d,
+                               float eps) {
     for (int i_point = threadIdx.x + blockIdx.x * blockDim.x; i_point < n; i_point += blockDim.x * gridDim.x) {
+        int number_of_neighbors = 0;
         for (int j_point = 0; j_point < n; j_point++) {
             float distance = euclidian_distance(i_point, j_point, d_data, d);
             if (distance <= eps) {
-                d_neighborhood_sizes[i_point]++;
+                number_of_neighbors++;
             }
         }
+        d_neighborhood_sizes[i_point] = number_of_neighbors;
     }
 }
 
@@ -92,6 +96,47 @@ void kernel_expand_clusters(int *d_C, int *d_changed, int *d_neighborhood_points
 
 
 __global__
+void kernel_expand_clusters_CG(
+        int *d_C, int *d_changed, int *d_neighborhood_points, int *d_neighborhood_ends, int *d_neighborhood_sizes,
+        int n, int minPts
+) {
+
+    auto g = cg::this_grid();
+
+    for (int i_point = threadIdx.x + blockIdx.x * blockDim.x; i_point < n; i_point += blockDim.x * gridDim.x) {
+        d_C[i_point] = d_neighborhood_sizes[i_point] >= minPts ? i_point : -1;
+    }
+
+    if (threadIdx.x + blockIdx.x * blockDim.x == 0) {
+        d_changed[0] = 1;
+    }
+    g.sync();
+
+    while (d_changed[0]) {
+        g.sync();
+        if (threadIdx.x + blockIdx.x * blockDim.x == 0) {
+            d_changed[0] = 0;
+        }
+        g.sync();
+
+        for (int i_point = threadIdx.x + blockIdx.x * blockDim.x; i_point < n; i_point += blockDim.x * gridDim.x) {
+            int neighborhood_start = get_start(d_neighborhood_ends, i_point);
+            int neighborhood_end = get_end(d_neighborhood_ends, i_point);
+            for (int i_neighbor = neighborhood_start; i_neighbor < neighborhood_end; i_neighbor++) {
+                int j_point = d_neighborhood_points[i_neighbor];
+                if (d_neighborhood_sizes[j_point] >= minPts &&
+                    d_C[j_point] > d_C[i_point]) {//todo could be replace with just checking that C is positive
+                    d_C[i_point] = d_C[j_point];
+                    d_changed[0] = 1;
+                }
+            }
+        }
+        g.sync();
+    }
+}
+
+
+__global__
 void kernel_expand_cluster(int *d_exp, int *d_changed, int *d_neighborhood_points, int *d_neighborhood_ends,
                            int *d_neighborhood_sizes,
                            int n, int minPts) {
@@ -127,7 +172,11 @@ void GPU_DBSCAN(int *h_C, float *h_data, int n, int d, float eps, int minPts) {
     kernel_neighborhood_sizes<<< number_of_blocks, number_of_threads >>>(
             d_neighborhood_sizes, d_data, n, d, eps
     );
+    cudaDeviceSynchronize();
+    gpuErrchk(cudaPeekAtLastError());
     inclusive_scan(d_neighborhood_sizes, d_neighborhood_ends, n);
+    cudaDeviceSynchronize();
+    gpuErrchk(cudaPeekAtLastError());
     int total_number_of_neighbors = copy_last_D_to_H(d_neighborhood_ends, n);
     d_neighborhood_points = gpu_malloc_int(total_number_of_neighbors);
     kernel_neighborhood_populate<<< number_of_blocks, number_of_threads >>>(
@@ -138,17 +187,39 @@ void GPU_DBSCAN(int *h_C, float *h_data, int n, int d, float eps, int minPts) {
     gpuErrchk(cudaPeekAtLastError());
 
     ///compute clustering
-    kernel_init_singleton_clusters<<< number_of_blocks, number_of_threads >>>(
-            d_C, d_neighborhood_sizes, n, minPts
+//    kernel_init_singleton_clusters<<< number_of_blocks, number_of_threads >>>(
+//            d_C, d_neighborhood_sizes, n, minPts
+//    );
+//    int changed = 1;
+//    while (changed) {
+//        gpu_set_all_zero(d_changed, 1);
+//        kernel_expand_clusters<<< number_of_blocks, number_of_threads >>>(
+//                d_C, d_changed, d_neighborhood_points, d_neighborhood_ends, d_neighborhood_sizes, n, minPts
+//        );
+//        changed = copy_last_D_to_H(d_changed, 1);
+//    }
+
+    void *params[] = {
+            &d_C, &d_changed, &d_neighborhood_points, &d_neighborhood_ends, &d_neighborhood_sizes,
+            &n, &minPts
+    };
+    cudaDeviceProp deviceProp;
+    cudaGetDeviceProperties(&deviceProp, 0);
+    int SMs = deviceProp.multiProcessorCount;
+
+    dim3 grid(SMs);
+    dim3 block(number_of_threads);
+    cudaLaunchCooperativeKernel(
+            (void *) kernel_expand_clusters_CG,
+            grid,
+            block,
+            params,
+            0,
+            0
     );
-    int changed = 1;
-    while (changed) {
-        gpu_set_all_zero(d_changed, 1);
-        kernel_expand_clusters<<< number_of_blocks, number_of_threads >>>(
-                d_C, d_changed, d_neighborhood_points, d_neighborhood_ends, d_neighborhood_sizes, n, minPts
-        );
-        changed = copy_last_D_to_H(d_changed, 1);
-    }
+    cudaDeviceSynchronize();
+    gpuErrchk(cudaPeekAtLastError());
+
 
     copy_D_to_H(h_C, d_C, n);
 
@@ -187,7 +258,11 @@ void G_DBSCAN(int *h_C, float *h_data, int n, int d, float eps, int minPts) {
     kernel_neighborhood_sizes<<< number_of_blocks, number_of_threads >>>(
             d_neighborhood_sizes, d_data, n, d, eps
     );
+    cudaDeviceSynchronize();
+    gpuErrchk(cudaPeekAtLastError());
     inclusive_scan(d_neighborhood_sizes, d_neighborhood_ends, n);
+    cudaDeviceSynchronize();
+    gpuErrchk(cudaPeekAtLastError());
     int total_number_of_neighbors = copy_last_D_to_H(d_neighborhood_ends, n);
     d_neighborhood_points = gpu_malloc_int(total_number_of_neighbors);
     kernel_neighborhood_populate<<< number_of_blocks, number_of_threads >>>(
